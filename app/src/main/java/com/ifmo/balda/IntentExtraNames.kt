a9a89056a12e21e0a9e66981bc873f277b9d4961
Boris Pristupa <boris.pristupa@jetbrains.com>
2023-03-14 23:54:33 +0200

    add mock game activity

--- app/src/main/java/com/ifmo/balda/IntentExtraNames.kt
+++ app/src/main/java/com/ifmo/balda/IntentExtraNames.kt
@@ -3,5 +3,7 @@ package com.ifmo.balda
 class IntentExtraNames {
   companion object {
     const val GAME_MODE = "GAME_MODE"
+    const val PLAYER_1_NAME = "PLAYER_1_NAME"
+    const val PLAYER_2_NAME = "PLAYER_2_NAME"
   }
 }
