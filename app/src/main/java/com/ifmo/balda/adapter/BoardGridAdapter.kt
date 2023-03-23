c2a8d3e52364a139dba78faa9dbce876d0a07527
Ekaterina Berezhko <Ekaterina.Berezhko@jetbrains.com>
2023-03-23 11:35:56 +0200

    Add Toast import

--- app/src/main/java/com/ifmo/balda/adapter/BoardGridAdapter.kt
+++ app/src/main/java/com/ifmo/balda/adapter/BoardGridAdapter.kt
@@ -7,6 +7,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
 import android.widget.ToggleButton
 import com.ifmo.balda.R
 
