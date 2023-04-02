d2888e163299c75358e36392c07c2dba78118178
MrFang <bat312@yandex.ru>
2023-04-02 21:32:25 +0300

    Load stat from db

--- app/src/main/java/com/ifmo/balda/activity/StatScreenActivity.kt
+++ app/src/main/java/com/ifmo/balda/activity/StatScreenActivity.kt
@@ -7,21 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
 import com.ifmo.balda.R
 import com.ifmo.balda.adapter.StatListAdapter
+import com.ifmo.balda.db
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class StatScreenActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_stat_screen)
 
-    findViewById<ListView>(R.id.statList).adapter = StatListAdapter(
-      this,
-      listOf( // TODO: get from data storage
-        Pair("Monkey King", 100500),
-        Pair("Lich", 100300),
-        Pair("Sniper", 100000),
-        Pair("Invoker", 3)
-      )
-    )
+    CoroutineScope(Dispatchers.IO).launch {
+      val stat = db.statDao().getStat()
+      runOnUiThread {
+        findViewById<ListView>(R.id.statList).adapter = StatListAdapter(
+          this@StatScreenActivity,
+          stat.map { Pair(it.name, it.score) }
+        )
+      }
+    }
+
 
     findViewById<ImageButton>(R.id.backButton).setOnClickListener {
       NavUtils.navigateUpFromSameTask(this)
